57a014068b79ec3a58b46da8a86a65777cbb408a
Jan Lescain <jmlescain@gmail.com>
2019-06-10 23:57:16 +0800

    intial commit, figured out socket.io connections

--- app.ino
+++ app.ino
@@ -1,11 +1,44 @@
-#include <WebSockets.h>
-#include <WebSocketsClient.h>
-#include <WebSocketsServer.h>
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <SocketIoClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIoClient webSocket;
+
+void event(const char * payload, size_t length) {
+  Serial.printf("got message: %s\n", payload);
+}
 
 void setup() {
-	
+    Serial.begin(115200);
+
+    Serial.setDebugOutput(true);
+
+    Serial.println();
+    Serial.println();
+    Serial.println();
+
+      for(uint8_t t = 4; t > 0; t--) {
+          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+          Serial.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("Flare S3 Power", "rageagainstthedyingofthelight");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    webSocket.on("event", event);
+    webSocket.begin("192.168.43.203", 3000);
 }
 
 void loop() {
-	
+    webSocket.loop();
+		delay(1000);
+		webSocket.emit("greeting", "\"the esp8266 say hi!\"");
 }
