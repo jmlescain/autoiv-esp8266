e7e34f99ef514c33b1ea70c7d7c9438115d52708
Jan Lescain <jmlescain@gmail.com>
2019-08-21 20:58:15 +0800

    WEIGHT ONLY

--- app.ino
+++ app.ino
@@ -3,16 +3,23 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
+#include <HX711.h>
+
 #include <SocketIoClient.h>
 
 ESP8266WiFiMulti WiFiMulti;
 SocketIoClient webSocket;
 
 unsigned long pulse_duration; //record the duration of low pulse
+//unsigned long weight_value;
 bool isFirstTimeConnect = true;
 
 //PINS
 int IR_in = 5;
+//const int LOADCELL_DOUT_PIN = 14;
+//const int LOADCELL_SCK_PIN = 12;
+
+//HX711 scale;
 
 void event(const char * payload, size_t length) {
   Serial.printf("got message: %s\n", payload);
@@ -28,17 +35,25 @@ void connect(const char * payload, size_t length) {
   webSocket.emit("identify", identity);
 }
 
-const char* readPulseIn(){
+const char* readValues(){
   pulse_duration = pulseIn(IR_in, LOW); //measure the duration of the low pulse
   String pulse_duration_string = (String) "\"" + pulse_duration + "\"";
   const char * pulse_duration_char = pulse_duration_string.c_str();
   //Serial.println(pulse_duration_char);
   webSocket.emit("pulseIn", pulse_duration_char);
+  // weight_value = scale.get_units(10);
+  // String weight_value_string = (String) "\"" + weight_value + "\"";
+  // const char * weight_value_char = weight_value_string.c_str();
+  // webSocket.emit("weighIn", weight_value_char);
 }
 
 
 void setup() {
     Serial.begin(115200);
+
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.set_scale(-1063.64);
+
 
     Serial.setDebugOutput(true);
 
@@ -71,14 +86,14 @@ void setup() {
 
     webSocket.on("event", event);
     webSocket.on("connect", connect);
-    webSocket.begin("192.168.0.11", 4000);
+    webSocket.begin("autoiv.xyz");
     connect("", 0);
 }
 
 void loop() {
     webSocket.loop();
     //webSocket.emit("pulseIn", readPulseIn());
-    readPulseIn();
+    readValues();
     //webSocket.emit("hello", "\"hello\"");
 }
 
