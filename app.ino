7c253a8b567056439cdf709ea45f6089bf8b4ec1
Jan Lescain <jmlescain@gmail.com>
2019-10-08 16:31:58 +0800

    additional pulse things

--- app.ino
+++ app.ino
@@ -11,8 +11,10 @@ ESP8266WiFiMulti WiFiMulti;
 SocketIoClient webSocket;
 
 unsigned long pulse_duration; //record the duration of low pulse
+unsigned long prev_pulse;
 float weight_value;
 bool isConnected = false;
+int zero_count;
 
 //PINS
 int IR_in = 5;
@@ -45,13 +47,25 @@ void disconnect(const char * payload, size_t length){
 
 const char *readValues()
 {
-  pulse_duration = pulseIn(IR_in, LOW, 5000000); //measure the duration of the low pulse
-  if (pulse_duration >= 100000 || pulse_duration == 0) {
+  pulse_duration = pulseIn(IR_in, LOW, 7000000); //measure the duration of the low pulse
+  Serial.println(pulse_duration);
+  if (pulse_duration == 0 && prev_pulse == 0) {
+      zero_count++;
+      //Serial.println("zero count: " + zero_count);
+  } else {
+    zero_count = 0;
+  }
+  if (pulse_duration >= 100000 || zero_count == 6) {
+    if (zero_count == 6){
+      //pulse_duration = 0;
+      zero_count = 0;
+    }
     weight_value = scale.get_units(10);            //measure weight
     String payload = (String) "{\"pulse\": \"" + pulse_duration + "\",\"weight\": \"" + weight_value + "\"}";
     const char *payload_char = payload.c_str();
     webSocket.emit("values", payload_char);
   }
+  prev_pulse = pulse_duration;
 }
 
 void setup() {
@@ -60,8 +74,8 @@ void setup() {
     pinMode(IR_in, INPUT);
 
     scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    scale.set_scale(324.87);
-    scale.set_offset(211980);
+    scale.set_scale(325.65); //324.87
+    scale.set_offset(-121960); //211980
 
     Serial.setDebugOutput(true);
 
